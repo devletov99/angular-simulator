68ecbf27c899dc4cd7dedc5d34cc864a4f09c0d2
Девлетов Нурбек <ndevletov5@gmail.com>
2026-02-07 20:06:45 +0300

    tasks layout and angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,126 @@ import { Component } from '@angular/core';
 import './training';
 import { Color } from '../enums/Color';
 import './collection';
+import { FormsModule } from '@angular/forms';
+
+interface iService {
+  hover: boolean;
+  id: number;
+  img: string;
+  title: string;
+  description: string;
+}
+
+interface iLocation {
+  id: number;
+  location: string;
+}
+
+interface iParticipant {
+  id: number;
+  participant: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
 
-  companyName: String = 'румтибет';
+  companyName: string = 'румтибет';
+  selectedLocation!: string;
+  selectedParticipant!: string;
+  inputDate!: string;
+  selectedDate: Date = new Date();
+  counter: number = 0;
+  buttonText: string = 'показать счётчик';
+  isCounterVisible: boolean = false;
+  isTimeVisible: boolean = true;
+  liveInput!: string;
+  isPageVisible: boolean = true;
 
+  services: iService[] = [
+    {
+      hover: false,
+      id: 1,
+      img: 'leadership-icon',
+      title: 'Опытный гид',
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+    },
+    {
+       hover: false,
+      id: 2,
+      img: 'security-icon',
+      title: 'Безопасный поход',
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+    },
+    {
+      hover: false,
+      id: 3,
+      img: 'price-icon',
+      title: 'Лояльные цены',
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+    }
+  ];
+
+  locations: iLocation[] = [
+    {
+      id: 1,
+      location: 'Эльбрус',
+    },
+    {
+      id: 2,
+      location: 'Домбай',
+    },
+    {
+      id: 3,
+      location: 'Архыз',
+    },
+    {
+      id: 4,
+      location: 'Камчатка',
+    },
+    {
+      id: 5,
+      location: 'Дигория',
+    },
+    {
+      id: 6,
+      location: 'Алтай',
+    }
+  ];
+
+  participants: iParticipant[] = [
+    {
+      id: 1,
+      participant: '1-2',
+    },
+    {
+      id: 2,
+      participant: '3-4',
+    },
+    {
+      id: 3,
+      participant: '5-6',
+    },
+    {
+      id: 4,
+      participant: '7-8',
+    },
+    {
+      id: 5,
+      participant: '9-10',
+    },
+  ];
+
+  constructor() {
+    this.saveLastVisit();
+    this.saveQuantityVisit();
+
+  }
+  
   isMainColor(color: Color): boolean {
     const rgbColors: Color[] = [Color.RED, Color.GREEN, Color.BLUE];
     return rgbColors.includes(color);
@@ -29,9 +138,37 @@ export class AppComponent {
     localStorage.setItem('visit', String(visitNumber));
   }
 
-  constructor() {
-    this.saveLastVisit();
-    this.saveQuantityVisit();
+  isFormInvalid(): boolean {
+    return !this.selectedLocation || !this.inputDate || !this.selectedParticipant
   }
-  
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.selectedDate = new Date()
+    },1000);
+
+    setTimeout(() =>{
+      this.isPageVisible = false;
+    }, 2000)
+
+  }
+
+  buttonIncrease(): void {
+    this.counter = this.counter + 1;
+  }
+
+  buttonDecrease(): void {
+    this.counter = this.counter - 1;
+  }
+
+  disabledCounter(): boolean {
+    return this.counter <= 0;
+  }
+
+  changeText(): void {
+    this.buttonText = this.buttonText === 'показать счётчик' ? 'показать таймер' : 'показать счётчик';
+    this.isTimeVisible = !this.isTimeVisible;
+    this.isCounterVisible = !this.isCounterVisible;
+  }
+
 };
