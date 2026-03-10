c80dc0b0fc799aadb07c8d617fb89c014ff0c917
Девлетов Нурбек <ndevletov5@gmail.com>
2026-03-10 18:38:50 +0300

    tasks RxJs

---
+++ src/observable.ts
@@ -0,0 +1,27 @@
+import { filter, interval, map, Observable, of, take } from "rxjs";
+
+export const obs$ = new Observable<string>(observer => {
+  observer.next('Hello');
+	observer.next('RxJS');
+	observer.complete();
+}).subscribe({
+	next: (value) => console.log(value),
+	complete: () => console.log('Поток завершён'),
+})
+
+const num$ = of(1, 2, 3, 4, 5);
+
+num$.pipe(
+	map(num => num * 10),
+).subscribe(console.log);
+
+const evenNumbers$: Observable<number> = of(1, 2, 3, 4, 5, 6, 7, 8);
+
+evenNumbers$.pipe(
+	filter(num => num % 2 === 0),
+).subscribe(console.log);
+
+const inter$: Observable<number> = interval(1000).pipe(
+	take(5),
+);
+inter$.subscribe(nuber$ => console.log(nuber$));
